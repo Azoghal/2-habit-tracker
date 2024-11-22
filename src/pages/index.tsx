95a22bbd72ad516ac93cecff8076a772d51476c2
Sam Beer <sam.beer208@gmail.com>
2024-11-22 21:50:52 +0000

    hmm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,23 @@
-import { Box } from "@/components/Box";
 import Habits, { YEAR_MILLIS } from "@/components/Habits";
 import { Convert, IHabits } from "@/types/habits";
-import { useEffect, useMemo } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useCookies, CookiesProvider } from "react-cookie";
+
+import { StyledFirebaseAuth } from "react-firebaseui";
+import { firebaseApp, firebaseAuth } from "../firebase";
+
+import { GoogleAuthProvider } from "firebase/auth";
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: "popup",
+  signInOptions: [GoogleAuthProvider.PROVIDER_ID],
+  callbacks: {
+    // Avoid redirects after sign-in.
+    signInSuccessWithAuthResult: () => false,
+  },
+};
 
 const defaultHabits: IHabits = {
   title: "Habits of Sam",
@@ -22,6 +37,16 @@ const defaultHabits: IHabits = {
 const defaultHabitJson = Convert.habitsToJson(defaultHabits);
 
 export default function Index() {
+  const [isSignedIn, setIsSignedIn] = useState(false);
+
+  // Listen to the Firebase Auth state and set the local state.
+  useEffect(() => {
+    const unregisterAuthObserver = firebaseAuth.onAuthStateChanged((user) => {
+      setIsSignedIn(!!user);
+    });
+    return () => unregisterAuthObserver(); // Make sure we un-register Firebase observers when the component unmounts.
+  }, []);
+
   // TODO base64 it when we cookify it
   const [cookies, setCookie] = useCookies(["habitsCookie"]);
 
@@ -45,6 +70,20 @@ export default function Index() {
   // TODO should do something to verify the type is right.
   return (
     <CookiesProvider>
+      {isSignedIn ? (
+        <div>
+          <p>
+            Welcome {firebaseAuth.currentUser?.displayName ?? "Undefined Name"}!
+            You are now signed-in!
+          </p>
+          <a onClick={() => firebaseAuth.signOut()}>Sign-out</a>
+        </div>
+      ) : (
+        <div>
+          <p>Please sign-in:</p>
+          <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebaseAuth} />
+        </div>
+      )}
       {habitData && (
         <Habits
           data={habitData} // TODO this should be an IHabits instaed
