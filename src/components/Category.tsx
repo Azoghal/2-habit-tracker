eff3ffb8eed4cfd6ab1752675056380aeeb43697
Sam Beer <sam.beer208@gmail.com>
2024-12-16 23:47:08 +0000

    sort of working, but probably a couple of off by one errors

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -1,8 +1,8 @@
 import { useMemo, useState } from "react";
 import Row, { IHabitProps } from "./Row";
-import { IHabit } from "../types/habits";
+import { IActivity, IHabit } from "../types/habits";
 import { CheckboxStateFromInt, ICheckboxProps } from "./Checkbox";
-import { getTodayMidday } from "./Habits";
+import { DAY_SECONDS, getTodayMidday } from "./Habits";
 
 export interface ICategoryProps {
     title: string;
@@ -34,7 +34,50 @@ export default function Category(props: ICategoryProps) {
 
     const propifiedHabits = useMemo(() => {
         return props.habits.map((h) => {
-            const checkboxes = h.activities.map((a) => {
+            // todo here we need to:
+            // 1. have filtered out dates that aren't in the current range
+            // 2. have filled in dates that aren't in the current list
+
+            const hardCodedEarliest = today - 7 * DAY_SECONDS;
+            const hardCodedLatest = today + 7 * DAY_SECONDS;
+            const filtered = h.activities.filter((a) => {
+                return a.date >= hardCodedEarliest && a.date <= hardCodedLatest;
+            });
+            const datesWeHave = h.activities.map((a) => {
+                return a.date;
+            });
+
+            // TODO actually have all the days
+            // const datesWeNeedToAdd = [
+            //     today - DAY_SECONDS,
+            //     today,
+            //     today + DAY_SECONDS,
+            // ];
+
+            // TODO make sure this actually does what we want.
+            const datesWeNeed = Array.from(
+                { length: 15 },
+                (_, i) => (i - 7) * DAY_SECONDS + today,
+            );
+
+            // filter out the dates that don't appear
+            const datesToAdd = datesWeNeed.filter((d) => {
+                return !datesWeHave.includes(d);
+            });
+
+            console.log("toAdd: ", datesToAdd);
+
+            filtered.concat(
+                datesToAdd.map((date: number) => {
+                    const activity: IActivity = {
+                        date,
+                        value: 0,
+                    };
+                    return activity;
+                }),
+            );
+
+            const checkboxes = filtered.map((a) => {
                 const checkbox: ICheckboxProps = {
                     ...a,
                     state: CheckboxStateFromInt(a.value),
