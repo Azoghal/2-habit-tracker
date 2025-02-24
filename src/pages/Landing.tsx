1e8b518f2ab2cb7ad7d0cfa8249f3f71657a6915
Sam Beer <sam.beer208@gmail.com>
2025-02-24 00:03:04 +0000

    get rid of the nonsense at the top

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -4,13 +4,7 @@ import { useUserAuth } from "../context/SessionHelpers";
 function Landing(): JSX.Element {
     const user = useUserAuth();
 
-    return (
-        <>
-            <h1>Firebase Demo - Recipes</h1>
-            <h2>{user?.email}</h2>
-            {user ? <EHabitsTableMaster user={user} /> : <>loading user</>}
-        </>
-    );
+    return <>{user ? <EHabitsTableMaster user={user} /> : <>loading user</>}</>;
 }
 
 export default Landing;
